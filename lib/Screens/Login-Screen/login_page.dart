d8e16bd875e580f62521996aaf819801e78c16ae
samuelson <samuelsonowei04@gmail.com>
2023-01-03 22:20:35 +0100

    Updated

---
+++ lib/Screens/Login-Screen/login_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:roland_salary_website/Screens/Home-Screen/home_page.dart';
+import 'package:roland_salary_website/widgets/text_fields.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              homePageButton(context),
+              const SizedBox(
+                height: 70,
+              ),
+              Container(
+                padding: EdgeInsets.zero,
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: GoogleFonts.ptSans(
+                      fontSize: 55,
+                      color: const Color(0xFF2B1347),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Center(
+                  child: Text(
+                    "Welcome Back!, please sign in with your credendials",
+                    style: GoogleFonts.ptSans(
+                        fontSize: 20,
+                        color: const Color(0xFF2B1347),
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text(
+                  " Email address",
+                  style: GoogleFonts.ptSans(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1347)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: emailField(),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text(
+                  " Password",
+                  style: GoogleFonts.ptSans(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1347)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: passwordField(),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 35),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      child: Text(
+                        "Forgot Password?",
+                        style: GoogleFonts.ptSans(
+                            color: const Color(0xFF2B1347), fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: GestureDetector(
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    decoration: BoxDecoration(
+                        color: const Color(
+                          0xFF2B1347,
+                        ),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Center(
+                      child: Text(
+                        "Log in",
+                        style: GoogleFonts.ptSans(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 30,
+                  right: 30,
+                ),
+                color: Colors.transparent,
+                height: 20,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Not a member?",
+                        style: GoogleFonts.ptSans(
+                          color: Colors.black54,
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      GestureDetector(
+                        child: Text(
+                          "Create Account",
+                          style: GoogleFonts.ptSans(
+                              color: const Color(0xFF2B1347),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector homePageButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+          (route) => false),
+      child: Container(
+        padding: EdgeInsets.zero,
+        height: 35,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Image.asset(
+            "assets/images/image1.png",
+            height: 150,
+            width: 125,
+          ),
+        ),
+      ),
+    );
+  }
+}

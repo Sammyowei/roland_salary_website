29a9c0843c4352d2269ef3359d12408ef641e09e
samuelson <samuelsonowei04@gmail.com>
2023-01-09 13:02:22 +0100

    Updated

--- lib/Screens/sign-up-screen/signup.dart
+++ lib/Screens/sign-up-screen/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:roland_salary_website/Screens/Dash-Doard-Screen/constants.dart';
 import 'package:roland_salary_website/Screens/Dash-Doard-Screen/dashboard_screen.dart';
 import 'package:roland_salary_website/Screens/Home-Screen/home_page.dart';
 import 'package:roland_salary_website/Screens/Login-Screen/login_page.dart';
@@ -17,240 +18,281 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          controller: _scrollController,
-          physics: const AlwaysScrollableScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              homeButton(context),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: const EdgeInsets.only(
-                  left: 20,
-                  right: 20,
+      body: Form(
+        key: formKey,
+        child: SafeArea(
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                homeButton(context),
+                const SizedBox(
+                  height: 30,
                 ),
-                child: Image.asset(
-                  "assets/images/imqge6.png",
-                  height: 200,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Center(
-                  child: Text(
-                    "Welcome to smartpayy - Getting paid online just got better.  Sign up now to enjoy seamless transactions globally.",
-                    style: GoogleFonts.ptSans(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: const Color(0xFF2B1330).withOpacity(0.5),
-                    ),
-                    textAlign: TextAlign.center,
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: Image.asset(
+                    "assets/images/imqge6.png",
+                    height: 200,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              Container(
-                padding: EdgeInsets.zero,
-                child: Center(
+                Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Center(
+                    child: Text(
+                      "Welcome to smartpayy - Getting paid online just got better.  Sign up now to enjoy seamless transactions globally.",
+                      style: GoogleFonts.ptSans(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: const Color(0xFF2B1330).withOpacity(0.5),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  padding: EdgeInsets.zero,
+                  child: Center(
+                    child: Text(
+                      "Sign up",
+                      style: GoogleFonts.ptSans(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                        color: const Color(0xFF2B1330),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
                   child: Text(
-                    "Sign up",
+                    "First Name",
                     style: GoogleFonts.ptSans(
-                      fontSize: 35,
                       fontWeight: FontWeight.w600,
                       color: const Color(0xFF2B1330),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "First Name",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
-                  ),
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: firstNameField(),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "Last name",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: lastNameField(),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "Email address",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: emailField(),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "Username",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: userNameField(),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "Phone Number",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
+                Padding(
                   padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: phoneNumberField()),
-              const SizedBox(
-                height: 010,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 30),
-                child: Text(
-                  "Country",
-                  style: GoogleFonts.ptSans(
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF2B1330),
+                  child: firstNameField(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Last name",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Padding(
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
                   padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: countryField()),
-              const SizedBox(
-                height: 50,
-              ),
-              signupButtonToDashboard(context),
-              const SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: Container(
-                    padding: const EdgeInsets.only(),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Already a member?",
-                          style: GoogleFonts.ptSans(
-                            color: const Color(0xFF2B1330),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 3,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushAndRemoveUntil(
-                                MaterialPageRoute(
-                              builder: (context) {
-                                return const LoginPage();
-                              },
-                            ), (route) => false);
-                          },
-                          child: Text(
-                            "Log in ",
+                  child: lastNameField(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Email address",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: emailField(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Username",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: userNameField(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Phone Number",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: phoneNumberField()),
+                const SizedBox(
+                  height: 010,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Country",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: countryField()),
+                const SizedBox(
+                  height: 010,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Password",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: passwordField()),
+                const SizedBox(
+                  height: 010,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 30),
+                  child: Text(
+                    "Confirm password",
+                    style: GoogleFonts.ptSans(
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF2B1330),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: confirmPasswordField()),
+                const SizedBox(
+                  height: 50,
+                ),
+                signupButtonToDashboard(context),
+                const SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: Container(
+                      padding: const EdgeInsets.only(),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already a member?",
                             style: GoogleFonts.ptSans(
                               color: const Color(0xFF2B1330),
                               fontSize: 16,
-                              fontWeight: FontWeight.w700,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
-                        ),
-                      ],
-                    )),
-              ),
-              const SizedBox(
-                height: 70,
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(),
-                  child: Text(
-                    "© All right reserved 2022",
-                    style: GoogleFonts.ptSans(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black.withOpacity(0.5)),
-                    textAlign: TextAlign.center,
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                builder: (context) {
+                                  return const LoginPage();
+                                },
+                              ), (route) => false);
+                            },
+                            child: Text(
+                              "Log in ",
+                              style: GoogleFonts.ptSans(
+                                color: const Color(0xFF2B1330),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(),
+                    child: Text(
+                      "© All right reserved 2022",
+                      style: GoogleFonts.ptSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black.withOpacity(0.5)),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              )
-            ],
+                const SizedBox(
+                  height: 30,
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -261,14 +303,16 @@ class _SignUpPageState extends State<SignUpPage> {
     return Center(
       child: GestureDetector(
         onTap: () {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content:
-                  Text("Sign up successful, taking you to your dashboard!")));
-          Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(
-            builder: (context) {
-              return const DashboardScreen();
-            },
-          ), (route) => false);
+          if (formKey.currentState!.validate()) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content:
+                    Text("Sign up successful, taking you to your dashboard!")));
+            Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(
+              builder: (context) {
+                return const DashboardScreen();
+              },
+            ), (route) => false);
+          }
         },
         child: Container(
           height: 40,
--- lib/widgets/text_fields.dart
+++ lib/widgets/text_fields.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 final emailController = TextEditingController();
@@ -13,6 +15,15 @@ TextFormField emailField() {
   final emailController = TextEditingController();
   return TextFormField(
     controller: emailController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your email address";
+      } else if (!value.contains(RegExp(
+          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"))) {
+        return "invalid email format";
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -30,6 +41,14 @@ TextFormField userNameField() {
   // final userNameController = TextEditingController();
   return TextFormField(
     controller: userNameController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your user name";
+      } else if (!value.contains(RegExp(r"^[a-z0-9.!#$%&'*+/=?^_`{|}~-]"))) {
+        return "username must be lowercase ";
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -46,7 +65,15 @@ TextFormField userNameField() {
 TextFormField phoneNumberField() {
   // final passwordController = TextEditingController();
   return TextFormField(
-    controller: passwordController,
+    controller: phoneNumberController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your phone number";
+      } else if (!value.contains(RegExp(r"^[+0-9]"))) {
+        return "phone number is invalid";
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -54,7 +81,7 @@ TextFormField phoneNumberField() {
             color: Color(0XFF2B1347),
           ),
         ),
-        hintText: "Phone number",
+        hintText: "+12345....",
         alignLabelWithHint: true,
         fillColor: const Color(0xff2B1347)),
   );
@@ -64,6 +91,14 @@ TextFormField firstNameField() {
   // final firstnameController = TextEditingController();
   return TextFormField(
     controller: firstNameController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your first name";
+      } else if (!value.contains(RegExp(r"^[a-zA-Z]"))) {
+        return "invalid name format ";
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -81,6 +116,14 @@ TextFormField lastNameField() {
   // final lastNameController = TextEditingController();
   return TextFormField(
     controller: lastNameController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your last name";
+      } else if (!value.contains(RegExp(r"^[a-zA-Z]"))) {
+        return "invalid name format ";
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -98,6 +141,16 @@ TextFormField passwordField() {
   // final passwordController = TextEditingController();
   return TextFormField(
     controller: passwordController,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your password";
+      } else if (!value.contains(RegExp(
+          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-][a-zA-Z0-9@](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"))) {
+        return "invalid password format";
+      }
+      return null;
+    },
+    obscureText: true,
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -114,6 +167,14 @@ TextFormField passwordField() {
 TextFormField countryField() {
   // final countryController = TextEditingController();
   return TextFormField(
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your country name";
+      } else if (!value.contains(RegExp(r"^[a-zA-Z]"))) {
+        return "invalid country name";
+      }
+      return null;
+    },
     controller: countryController,
     decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -122,7 +183,7 @@ TextFormField countryField() {
             color: Color(0XFF2B1347),
           ),
         ),
-        hintText: "Select country",
+        hintText: "Country",
         alignLabelWithHint: true,
         fillColor: const Color(0xff2B1347)),
   );
@@ -132,6 +193,20 @@ TextFormField confirmPasswordField() {
   // final countryCode = TextEditingController();
   return TextFormField(
     controller: confirmPasswordController,
+    obscureText: true,
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "please enter your password";
+      } else if (!value.contains(RegExp(
+          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-][a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"))) {
+        return "invalid password format";
+      } else if (value.isNotEmpty) {
+        if (value != passwordController.text) {
+          return "password does not match";
+        }
+      }
+      return null;
+    },
     decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
@@ -143,4 +218,16 @@ TextFormField confirmPasswordField() {
         alignLabelWithHint: true,
         fillColor: const Color(0xff2B1347)),
   );
+}
+
+bool isPasswordCorrect(String message) {
+  if (passwordController.text.trim() == confirmPasswordController.text.trim()) {
+    message = "Password is correct";
+    log(message);
+    return true;
+  } else {
+    message = "Password is incorrect";
+    log(message);
+    return false;
+  }
 }
